f6de4e450dfac75172286a09db3686b15362f072
Manuel <c.manuelinares@gmail.com>
2025-05-19 12:25:22 -0500

    Pequeñas correciones a archivos y estructura

--- lib/core/constants/api_endpoints.dart
+++ lib/core/constants/api_endpoints.dart
@@ -1,28 +1,34 @@
 import 'config.dart';
 
 class ApiEndpoints {
+  //Clientes
+  static const String actualizarCliente = "$baseUrl/clientes/actualizar_cliente.php";
   static const String registrarCliente = "$baseUrl/clientes/registrar_cliente.php";
+  static const String eliminarCliente = "$baseUrl/clientes/eliminar_cliente.php";
   static const String obtenerClientes = "$baseUrl/clientes/obtener_clientes.php";
   static const String historialVentasCliente = "$baseUrl/clientes/historial_ventas_cliente.php";
   static const String historialPagosCliente = "$baseUrl/clientes/historial_pagos_cliente.php";
   static const String clientesEndeudados = "$baseUrl/clientes/clientes_endeudados.php";
+  //Inventario
   static const String resumenVentasCliente = "$baseUrl/inventario/resumen_ventas_cliente.php";
   static const String resumenInventario = "$baseUrl/inventario/resumen_inventario.php";
+  //Lotes
   static const String detallesLote = "$baseUrl/lotes/detalles_lote.php";
   static const String obtenerLotes = "$baseUrl/lotes/obtener_lotes.php";
   static const String historialIngresosLote = "$baseUrl/lotes/historial_ingresos_lote.php";
   static const String registrarLote = "$baseUrl/lotes/registrar_lote.php";
   static const String eliminarLote = "$baseUrl/lotes/eliminar_lote.php";
+  //Recepción
   static const String detallesRecepcion = "$baseUrl/recepciones/detalles_recepcion.php";
   static const String obtenerRecepciones = "$baseUrl/recepciones/obtener_recepciones.php";
   static const String registrarRecepcion = "$baseUrl/recepciones/registrar_recepcion.php";
   static const String eliminarRecepcion = "$baseUrl/recepciones/eliminar_recepcion.php";
+  //Ventas
   static const String ventasPendientes = "$baseUrl/ventas/ventas_pendientes.php";
   static const String registrarVenta = "$baseUrl/ventas/registrar_venta.php";
   static const String obtenerVentas = "$baseUrl/ventas/obtener_ventas.php";
-  static const String actualizarCliente = "$baseUrl/clientes/actualizar_cliente.php";
-  static const String eliminarCliente = "$baseUrl/clientes/eliminar_cliente.php";
   static const String eliminarVenta = "$baseUrl/ventas/eliminar_venta.php";
+  //Productos
   static const String registrarProducto = "$baseUrl/productos/registrar_producto.php";
   static const String obtenerProductos = "$baseUrl/productos/obtener_productos.php";
   static const String actualizarProducto = "$baseUrl/productos/actualizar_producto.php";
